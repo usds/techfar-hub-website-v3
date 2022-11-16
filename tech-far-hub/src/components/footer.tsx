8a169e58cc74f639766e9d6b246aff4d1b96eede
matt bowen <matt@mattbowen.net>
2022-11-16 11:49:37 -0500

    v3 48 Add social icons

--- tech-far-hub/src/components/footer.tsx
+++ tech-far-hub/src/components/footer.tsx
@@ -1,8 +1,14 @@
 import * as React from "react"
 
-import { GridContainer, Grid, FooterNav } from '@trussworks/react-uswds'
+import { GridContainer, Grid, FooterNav, SocialLink, SocialLinks } from '@trussworks/react-uswds'
 
 const Footer = () => {
+    const socialLinks = [
+        <SocialLink key="facebook" name="Facebook" href="#" />,
+        <SocialLink key="twitter" name="Twitter" href="#" />,
+        <SocialLink key="youtube" name="YouTube" href="#" />,
+        <SocialLink key="instagram" name="Instagram" href="#" />,
+    ]
     return (
         <footer className="usa-footer usa-footer--big">
             <GridContainer className="usa-footer__return-to-top">
@@ -14,7 +20,7 @@ const Footer = () => {
                         <Grid row gap={2}>
                             <Grid tablet={{ col: 2 }}>
                                 <Grid row>
-                                <img src="/images/usds-logo-footer.svg" alt="United States Digital Service" />
+                                    <img src="/images/usds-logo-footer.svg" alt="United States Digital Service" />
                                 </Grid>
                                 <Grid row>
                                     U.S. Digital Service
@@ -22,23 +28,41 @@ const Footer = () => {
                             </Grid>
                             <Grid tablet={{ col: 2 }}>
                                 <FooterNav
-        aria-label="Footer navigation"
-        size="big"
-        links={[
-            <a href="#">Nav link 1</a>,
-            <a href="#">Nav link 1</a>
+                                    aria-label="Footer navigation"
+                                    size="big"
+                                    links={[
+                                        <a href="#">Nav link 1.1</a>,
+                                        <a href="#">Nav link 1.1</a>
 
-        ]}
-        />
+                                    ]}
+                                />
                             </Grid>
                             <Grid tablet={{ col: 2 }}>
-                                Three
+                                <FooterNav
+                                    aria-label="Footer navigation"
+                                    size="big"
+                                    links={[
+                                        <a href="#">Nav link 2.1</a>,
+                                        <a href="#">Nav link 2.1</a>
+
+                                    ]}
+                                />
+
                             </Grid>
                             <Grid tablet={{ col: 2 }}>
-                                Four
+                                <FooterNav
+                                    aria-label="Footer navigation"
+                                    size="big"
+                                    links={[
+                                        <a href="#">Nav link 3.1</a>,
+                                        <a href="#">Nav link 3.1</a>
+
+                                    ]}
+                                />
+
                             </Grid>
                             <Grid tablet={{ col: 3 }}>
-                                Social
+                                 <SocialLinks links={socialLinks} />
                             </Grid>
                         </Grid>
                     </Grid>
