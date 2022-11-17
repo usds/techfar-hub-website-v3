62240ae2d3bf0f528dfe5bcffec596480a7e5b42
matt bowen <matt@mattbowen.net>
2022-11-17 10:22:22 -0500

    v3 48 Setup prefixing for staging deploys

--- tech-far-hub/gatsby-config.ts
+++ tech-far-hub/gatsby-config.ts
@@ -3,13 +3,13 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `TechFAR Hub`,
-    siteUrl: `https://usds.github.io/techfar-hub-website-v3`
+    siteUrl: process.env.SITE_URL || 'http://localhost:8000',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: "techfar-hub-website-v3",
+  pathPrefix: process.env.DEPLOY_PREFIX || '',
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
