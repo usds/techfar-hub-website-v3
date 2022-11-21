4ed1087c17568290ab4481036d21e6bf2f4939f6
matt bowen <matt@mattbowen.net>
2022-11-21 11:22:48 -0500

    v3 51 Fix types on page creation query

--- tech-far-hub/gatsby-node.ts
+++ tech-far-hub/gatsby-node.ts
@@ -1,11 +1,41 @@
 import type { GatsbyNode } from "gatsby"
 import path from "path"
 
-export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions, reporter }) => {
-    const { createPage } = actions
 
-    const result = await graphql(
-        `
+interface ITemplatedNode {
+    id: string,
+    parent: {
+      id: string,
+      name: string,
+      base: string,
+      relativeDirectory: string,
+      relativePath: string,
+    },
+    frontmatter: {
+      slug?: string,
+    },
+    internal: {
+      contentFilePath: string
+    }
+}
+
+
+interface ITemplateNodeResultSet {
+  allMdx: {
+    edges: ReadonlyArray<{readonly node: ITemplatedNode}>
+    
+  }
+}
+
+interface IGraphQLTemplateNodeResult {
+  errors?: any,
+  data?: ITemplateNodeResultSet,
+}
+
+export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result:IGraphQLTemplateNodeResult = await graphql(`
 {
   allMdx {
     edges {
@@ -31,30 +61,30 @@ export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions,
     }
   }
 }
-        `
-    )
-    if (result.errors) {
-        reporter.panicOnBuild(`Error while running GraphQL query.`)
-        return
-    }
+`)
 
-    result.data.allMdx.edges.forEach(({ node }) => {
-        const parentDirs: string = node.parent.relativeDirectory
-        const contentType: string = node.parent.relativeDirectory.split(path.sep)[0]
-        const pageName: string = node.parent.name
-        const pageIdentier: string = node.frontmatter.slug || pageName;
+  if (result.errors || !result.data) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
 
-        // TODO: Make page-specific templates more general
-        const templateName = pageName === 'index' ? 'template-index.tsx' : 'template-default.tsx'
-        const pagePath = pageName == 'index' ? `${parentDirs}/` : `${parentDirs}/${pageIdentier}/`
-        const template = path.resolve('src', 'pages', contentType, templateName)
-        createPage({
-            path: pagePath,
-            component: `${template}?__contentFilePath=${node.internal.contentFilePath}`,
-            context: {
-                id: node.id
-            }
-        })
+  result.data.allMdx.edges.forEach(({ node }: { node: ITemplatedNode }) => {
+    const parentDirs: string = node.parent.relativeDirectory
+    const contentType: string = node.parent.relativeDirectory.split(path.sep)[0]
+    const pageName: string = node.parent.name
+    const pageIdentier: string = node.frontmatter.slug || pageName;
 
-    });
+    // TODO: Make page-specific templates more general
+    const templateName = pageName === 'index' ? 'template-index.tsx' : 'template-default.tsx'
+    const pagePath = pageName == 'index' ? `${parentDirs}/` : `${parentDirs}/${pageIdentier}/`
+    const template = path.resolve('src', 'pages', contentType, templateName)
+    createPage({
+      path: pagePath,
+      component: `${template}?__contentFilePath=${node.internal.contentFilePath}`,
+      context: {
+        id: node.id
+      }
+    })
+
+  });
 }
