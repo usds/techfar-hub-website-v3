3691ad9e73f6b05fd6490e17f051c3909d27eead
matt bowen <matt@mattbowen.net>
2022-12-08 21:17:51 -0500

    v3 10 WIP OL -> Process List

--- tech-far-hub/src/components/mdxcontent.tsx
+++ tech-far-hub/src/components/mdxcontent.tsx
@@ -3,9 +3,10 @@ import { MDXProvider } from "@mdx-js/react";
 import { Alert } from "./alert";
 import { Summary } from "./summary";
 import ExternalResources from "./external-resources";
+import { ProcessList } from "./process-list";
 
 const MDXContent = ({ children }: { children: React.ReactNode }): JSX.Element => {
-  const components = { Alert, Summary, ExternalResources };
+  const components = { Alert, Summary, ExternalResources, ProcessList };
   return <MDXProvider components={components}>{children}</MDXProvider>;
 };
 
---
+++ tech-far-hub/src/components/process-list.tsx
@@ -0,0 +1,46 @@
+import { Alert, ProcessList as TWProcessList, ProcessListHeading, ProcessListItem } from "@trussworks/react-uswds";
+import * as React from "react";
+
+const processList = (children: React.ReactElement) => {
+  const errorMessage = (
+    <Alert type="warning" headingLevel="h3" heading="Error in process list">
+      A process list must be an ordered list.{" "}
+      <a href="https://www.markdownguide.org/basic-syntax/#ordered-lists">See the markdown guide for examples.</a>
+    </Alert>
+  );
+
+  if (children.type !== "ol") {
+    return errorMessage;
+  }
+
+  const elementFilter = (item: React.ReactNode) => typeof item == typeof {} && "type" in item;
+  let items = children.props.children.filter(elementFilter);
+
+  if (!items.every((item) => item.type == "li")) {
+    return errorMessage;
+  }
+  return (
+    <TWProcessList>
+      {items.map((li) => {
+        if (typeof li.props.children === "string") {
+          return (
+            <ProcessListItem>
+              <ProcessListHeading type="h4">{li.props.children}</ProcessListHeading>
+            </ProcessListItem>
+          );
+        } else {
+          const filteredChildren = li.props.children.filter(elementFilter);
+          return (
+            <ProcessListItem>
+              <ProcessListHeading type="h4">{filteredChildren[0].props.children}</ProcessListHeading>
+              {filteredChildren.length > 1 && filteredChildren.slice(1)}
+            </ProcessListItem>
+          );
+        }
+      })}
+    </TWProcessList>
+  );
+};
+export const ProcessList = ({ children }: { children: React.ReactElement }): JSX.Element => {
+  return <div className="process-list">{processList(children)}</div>;
+};
