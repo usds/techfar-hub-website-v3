20d2ef73881ee5879f46ae5e086af1b1396006fd
matt bowen <matt@mattbowen.net>
2022-12-14 11:37:47 -0500

    v3 85 Apply styling

--- tech-far-hub/src/components/multi-step.tsx
+++ tech-far-hub/src/components/multi-step.tsx
@@ -1,6 +1,6 @@
-import { Alert } from "@trussworks/react-uswds";
 import * as React from "react";
-import { json } from "stream/consumers";
+
+import { Alert, Card, CardBody } from "@trussworks/react-uswds";
 
 export const MultiStep = ({ children }: { children: React.ReactElement }) => {
   const errorMessage = (
@@ -14,27 +14,15 @@ export const MultiStep = ({ children }: { children: React.ReactElement }) => {
     return errorMessage;
   }
 
-  const elementFilter = (item: React.ReactNode) => typeof item == typeof {} && "type" in item;
+  const elementFilter = (item: React.ReactNode) => item && typeof item === "object" && "type" in item;
   let items = children.props.children.filter(elementFilter);
 
-  if (!items.every((item) => item.type == "li")) {
+  if (
+    !items.every((item: React.ReactNode) => item && typeof item === "object" && "type" in item && item.type == "li")
+  ) {
     return errorMessage;
   }
-
-  const headings: React.ReactNode[] = [];
-  const bodies: React.ReactNode[] = [];
-  items.forEach((li: React.ReactNode) => {
-    console.log("\nNode: ");
-    if (li && typeof li === "object" && "props" in li && "children" in li.props) {
-      const filteredChildren = li.props.children.filter(elementFilter);
-      console.log(filteredChildren);
-      console.log("Heading: ", filteredChildren[0]);
-      console.log("Bodies: ", filteredChildren.slice(1));
-      headings.push(filteredChildren[0]);
-      bodies.push(filteredChildren.slice(1));
-    }
-  });
-  const [isVisible, setIsVisible] = React.useState([true, ...Array(headings.length).fill(false)]);
+  const [isVisible, setIsVisible] = React.useState([true, ...Array(items.length).fill(false)]);
   const handleClick = (index: number): void => {
     setIsVisible(() => {
       const newIsVisible = Array(isVisible.length).fill(false);
@@ -43,29 +31,41 @@ export const MultiStep = ({ children }: { children: React.ReactElement }) => {
     });
   };
 
-  const headingElements = headings.map((node, index) => {
-    return (
-      <li
-        className={`usa-step-indicator__segment ${
-          isVisible[index] ? "usa-step-indicator__segment--current" : "usa-step-indicator__segment--complete"
-        }`}
-        onClick={() => handleClick(index)}
-      >
-        <div className="usa-step-indicator__segment-label">{node}</div>
-      </li>
-    );
+  const headings: React.ReactNode[] = [];
+  const bodies: React.ReactNode[] = [];
+  items.forEach((li: React.ReactNode, index: number) => {
+    console.log("\nNode: ");
+    if (li && typeof li === "object" && "props" in li && "children" in li.props) {
+      const filteredChildren = li.props.children.filter(elementFilter);
+      console.log(filteredChildren);
+      console.log("Heading: ", filteredChildren[0]);
+      console.log("Bodies: ", filteredChildren.slice(1));
+      headings.push(
+        <li
+          className={`usa-step-indicator__segment ${
+            isVisible[index] ? "usa-step-indicator__segment--current" : "usa-step-indicator__segment--complete"
+          }`}
+          onClick={() => handleClick(index)}
+        >
+          <div className="usa-step-indicator__segment-label">{filteredChildren[0]}</div>
+        </li>
+      );
+      bodies.push(
+        <>{isVisible[index] && <div className="tfh-multistep__content-item">{filteredChildren.slice(1)}</div>}</>
+      );
+    }
   });
-  const bodyElements = bodies.map((node, index) => {
-    return <>{isVisible[index] && <li className="tfh-multistep__content-item">{node}</li>}</>;
-  });
+
   return (
-    <div className="tfh-multistep">
-      <div className="usa-step-indicator" aria-label="progress">
-        <ol className="usa-step-indicator__segments">{headingElements}</ol>
-      </div>
-      <div className="tfh-multipstep__content-block">
-        <ol className="tfh-multistep__content-items">{bodyElements}</ol>
-      </div>
-    </div>
+    <Card>
+      <CardBody>
+        <div className="tfh-multistep">
+          <div className="usa-step-indicator" aria-label="progress">
+            <ol className="usa-step-indicator__segments">{headings}</ol>
+          </div>
+          <div className="tfh-multipstep__content-block">{bodies}</div>
+        </div>
+      </CardBody>
+    </Card>
   );
 };
