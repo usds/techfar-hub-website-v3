fc74cba081eb73f7270f3b3405e3fc463a9cb001
matt bowen <matt@mattbowen.net>
2022-12-16 10:29:04 -0500

    v3 92 rearrange and comment the code

--- tech-far-hub/src/components/assessment.tsx
+++ tech-far-hub/src/components/assessment.tsx
@@ -2,7 +2,10 @@ import * as React from "react";
 import { Card, CardBody, Alert, Fieldset, Radio, Form } from "@trussworks/react-uswds";
 import Children from "react-children-utilities";
 
+// Filter for getting rid of newlines and nulls in a list of nodes
 const elementFilter = (item: React.ReactNode) => item && typeof item === "object" && "type" in item;
+
+// Filter for getting OLs specifically
 const olTest = (node: React.ReactNode) =>
   node &&
   typeof node === "object" &&
@@ -12,62 +15,114 @@ const olTest = (node: React.ReactNode) =>
   "children" in node.props &&
   Array.isArray(node.props.children);
 
-const processScores = (lis: React.ReactElement[], score: Number) => {
-  // The goal here is to extract the score bounds from list-items that look like
-  // 1. Novice (9 - 18 total points)
-  // The key are the numbers between parens, formatted with a hypen.
-  return lis.map((li: React.ReactElement) => {
-    if (li.props.children.length >= 2) {
-      const rating = li.props.children.filter(elementFilter)[0];
-      const ratingText = Children.onlyText(rating);
-      const ratingRange = ratingText.match(/\(\s*(?<lowBound>[0-9]+)\s*-\s*(?<highBound>[0-9]+).* points/);
-      let lowBound, highBound;
-      if (ratingRange && ratingRange.groups) {
-        lowBound = Number(ratingRange.groups.lowBound);
-        highBound = Number(ratingRange.groups.highBound);
-        if (score >= lowBound && score <= highBound) {
-          return li;
-        }
-      }
-    }
-  });
-};
+export const Assessment = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  // Expects an ordered list of lists --- each item in the outer list is a page of questions, and the
+  // inner list are the options for the question.
+  // Expects a final <ScorePage> with the rubric
 
-export const AssessmentScore = ({
-  children,
-  scores = [],
-  clickHandler,
-}: {
-  children: React.ReactNode;
-  scores?: Number[];
-  clickHandler: Function;
-}) => {
-  const totalScore = scores.reduce((a, b) => +a + +b);
+  //Make sure we got roughly what we expected
   const errorMessage = (
-    <Alert type="warning" headingLevel="h3" heading="Error in score key">
-      An assessment score must contain an ordered list of score ranges
+    <Alert type="warning" headingLevel="h3" heading="Error in Assessment">
+      An assessment must be an ordered list with a score section at the end
       <a href="https://www.markdownguide.org/basic-syntax/#ordered-lists">See the markdown guide for examples.</a>
     </Alert>
   );
-  if (!(children && Array.isArray(children) && children.some(olTest))) {
+  if (!(children && Array.isArray(children) && children.length === 2)) {
     return errorMessage;
   }
+
+  // Make sure we got a score page too
+  let assessmentQuestions: React.ReactElement;
+  let score: React.ReactElement;
+  if (typeof children[0] === "object" && "type" in children[0] && children[0].type === "ol") {
+    assessmentQuestions = children[0];
+  } else {
+    return errorMessage;
+  }
+  if (typeof children[1] === "object") {
+    score = children[1];
+  } else {
+    return errorMessage;
+  }
+
+  // Grab all the pages of the assessment (the assessment items)
+  let items = assessmentQuestions.props.children.filter(elementFilter);
+
+  if (
+    !items.every((item: React.ReactNode) => item && typeof item === "object" && "type" in item && item.type == "li")
+  ) {
+    return errorMessage;
+  }
+
+  // Each assessment page will need its visibility and contribution to the score tracked
+  const [isVisible, setIsVisible] = React.useState([true, ...Array(items.length + 1).fill(false)]);
+  const [scores, setScores]: [Number[], React.Dispatch<React.SetStateAction<Number[]>>] = React.useState(
+    Array(items.length).fill(1)
+  );
+
+  const handleNextPrevClick = (index: number): void => {
+    setIsVisible(() => {
+      const newIsVisible = Array(isVisible.length).fill(false);
+      newIsVisible[index] = true;
+      return newIsVisible;
+    });
+  };
+
+  const handleUpdateScore = (index: number, points: number): void => {
+    setScores(() => {
+      const newScores = [...scores];
+      newScores[index] = points;
+      return newScores;
+    });
+  };
+
+  // Pull the items into their own "screens" with the options
+  const screens = items.map((item: React.ReactNode, index: number) => {
+    let nextQuestionText = "Next Question";
+    let nextQuestionLink: React.ReactElement;
+    let atEnd = false;
+    if (index === items.length - 1) {
+      nextQuestionText = "Complete Assessment";
+      atEnd = true;
+    }
+    nextQuestionLink = (
+      <a
+        onClick={() => {
+          handleNextPrevClick(index + 1);
+        }}
+        className="tfh-assessment__next-question-link"
+      >
+        {nextQuestionText}
+      </a>
+    );
+    return (
+      <>
+        {isVisible[index] && (
+          <AssessmentItem itemNumber={index} handleUpdate={handleUpdateScore} currentScores={scores}>
+            {item}
+          </AssessmentItem>
+        )}
+        {isVisible[index] && nextQuestionLink}
+      </>
+    );
+  });
+
+  // Clone the rubric page so we can pass in the current score and a handler
+  const newScore = (
+    <AssessmentScore
+      scores={scores}
+      children={score.props.children}
+      clickHandler={handleNextPrevClick}
+    ></AssessmentScore>
+  );
+  // Add the rubric page to the end
+  screens.push(<>{isVisible[items.length] && newScore}</>);
+
   return (
     <>
-      <h3>Total: {String(totalScore)} Points</h3>
-      {children.filter(elementFilter).map((item: React.ReactNode) => {
-        if (item) {
-          if (item && olTest(item) && typeof item === "object" && "props" in item) {
-            return processScores(item.props.children.filter(elementFilter), totalScore);
-          } else {
-            return item;
-          }
-        }
-      })}
-      <span onClick={() => clickHandler(0)} className="usa-button">
-        Retake Assessment
-      </span>
-      <span className="usa-clearfix"></span>
+      <Card className="tfh-assessment">
+        <CardBody>{screens}</CardBody>
+      </Card>
     </>
   );
 };
@@ -83,8 +138,10 @@ const AssessmentItem = ({
   handleUpdate: Function;
   currentScores: Number[];
 }): JSX.Element => {
+  // Each item is a list item that contains at least an ordered list of items
+  // The points for each item are its position orinally in the list
   const errorMessage = (
-    <Alert type="warning" headingLevel="h3" heading="Error in process list">
+    <Alert type="warning" headingLevel="h3" heading="Error in Assessment Item">
       An assessment item must contain an ordered list
       <a href="https://www.markdownguide.org/basic-syntax/#ordered-lists">See the markdown guide for examples.</a>
     </Alert>
@@ -107,6 +164,8 @@ const AssessmentItem = ({
     return errorMessage;
   }
 
+  // Extract each list item and turn it into a radio option that contributes
+  // to the overall score
   const processedChildren = itemChildren.map((child: React.ReactNode) => {
     if (child && olTest(child) && typeof child === "object" && "props" in child) {
       const rawAnswers = child.props.children.filter(elementFilter);
@@ -154,97 +213,64 @@ const AssessmentItem = ({
   return <form>{processedChildren}</form>;
 };
 
-export const Assessment = ({ children }: { children: React.ReactNode }): JSX.Element => {
+export const AssessmentScore = ({
+  children,
+  scores = [],
+  clickHandler,
+}: {
+  children: React.ReactNode;
+  scores?: Number[];
+  clickHandler: Function;
+}) => {
+  // Expects a rubric that has list items that each contain
+  // a score range
+  const totalScore = scores.reduce((a, b) => +a + +b);
   const errorMessage = (
-    <Alert type="warning" headingLevel="h3" heading="Error in process list">
-      An assessment must be an ordered list with a score section at the end
+    <Alert type="warning" headingLevel="h3" heading="Error in score key">
+      An assessment score must contain an ordered list of score ranges
       <a href="https://www.markdownguide.org/basic-syntax/#ordered-lists">See the markdown guide for examples.</a>
     </Alert>
   );
-  if (!(children && Array.isArray(children) && children.length === 2)) {
+  if (!(children && Array.isArray(children) && children.some(olTest))) {
     return errorMessage;
   }
-  let assessmentQuestions: React.ReactElement;
-  let score: React.ReactElement;
-  if (typeof children[0] === "object" && "type" in children[0] && children[0].type === "ol") {
-    assessmentQuestions = children[0];
-  } else {
-    return errorMessage;
-  }
-  if (typeof children[1] === "object") {
-    score = children[1];
-  } else {
-    return errorMessage;
-  }
-
-  let items = assessmentQuestions.props.children.filter(elementFilter);
-
-  if (
-    !items.every((item: React.ReactNode) => item && typeof item === "object" && "type" in item && item.type == "li")
-  ) {
-    return errorMessage;
-  }
-  const [isVisible, setIsVisible] = React.useState([true, ...Array(items.length + 1).fill(false)]);
-  const [scores, setScores]: [Number[], React.Dispatch<React.SetStateAction<Number[]>>] = React.useState(
-    Array(items.length).fill(1)
-  );
-  const handleNextPrevClick = (index: number): void => {
-    setIsVisible(() => {
-      const newIsVisible = Array(isVisible.length).fill(false);
-      newIsVisible[index] = true;
-      return newIsVisible;
-    });
-  };
-
-  const handleUpdateScore = (index: number, points: number): void => {
-    setScores(() => {
-      const newScores = [...scores];
-      newScores[index] = points;
-      return newScores;
-    });
-  };
-  const screens = items.map((item: React.ReactNode, index: number) => {
-    let nextQuestionText = "Next Question";
-    let nextQuestionLink: React.ReactElement;
-    let atEnd = false;
-    if (index === items.length - 1) {
-      nextQuestionText = "Complete Assessment";
-      atEnd = true;
-    }
-    nextQuestionLink = (
-      <a
-        onClick={() => {
-          handleNextPrevClick(index + 1);
-        }}
-        className="tfh-assessment__next-question-link"
-      >
-        {nextQuestionText}
-      </a>
-    );
-    return (
-      <>
-        {isVisible[index] && (
-          <AssessmentItem itemNumber={index} handleUpdate={handleUpdateScore} currentScores={scores}>
-            {item}
-          </AssessmentItem>
-        )}
-        {isVisible[index] && nextQuestionLink}
-      </>
-    );
-  });
-  const newScore = (
-    <AssessmentScore
-      scores={scores}
-      children={score.props.children}
-      clickHandler={handleNextPrevClick}
-    ></AssessmentScore>
-  );
-  screens.push(<>{isVisible[items.length] && newScore}</>);
   return (
     <>
-      <Card className="tfh-assessment">
-        <CardBody>{screens}</CardBody>
-      </Card>
+      <h3>Total: {String(totalScore)} Points</h3>
+      {children.filter(elementFilter).map((item: React.ReactNode) => {
+        if (item) {
+          if (item && olTest(item) && typeof item === "object" && "props" in item) {
+            return processScores(item.props.children.filter(elementFilter), totalScore);
+          } else {
+            return item;
+          }
+        }
+      })}
+      <span onClick={() => clickHandler(0)} className="usa-button">
+        Retake Assessment
+      </span>
+      <span className="usa-clearfix"></span>
     </>
   );
+};
+
+const processScores = (lis: React.ReactElement[], score: Number) => {
+  // The goal here is to extract the score bounds from list-items that look like
+  // 1. Novice (9 - 18 total points)
+  // The key are the numbers between parens, formatted with a hypen.
+  return lis.map((li: React.ReactElement) => {
+    if (li.props.children.length >= 2) {
+      const rating = li.props.children.filter(elementFilter)[0];
+      const ratingText = Children.onlyText(rating);
+      const ratingRange = ratingText.match(/\(\s*(?<lowBound>[0-9]+)\s*-\s*(?<highBound>[0-9]+).* points/);
+      let lowBound, highBound;
+      if (ratingRange && ratingRange.groups) {
+        lowBound = Number(ratingRange.groups.lowBound);
+        highBound = Number(ratingRange.groups.highBound);
+        if (score >= lowBound && score <= highBound) {
+          return li;
+        }
+      }
+    }
+  });
 };
