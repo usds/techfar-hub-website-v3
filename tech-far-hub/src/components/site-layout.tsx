5fe868b6e16025af1831812f9b8672a3de7442e9
matt bowen <matt@mattbowen.net>
2023-01-03 13:26:54 -0500

    v3 103 Add DAP

--- tech-far-hub/src/components/site-layout.tsx
+++ tech-far-hub/src/components/site-layout.tsx
@@ -61,6 +61,10 @@ const SiteLayout = ({ children, breadCrumbs, className }: ILayoutProps) => {
         </GridContainer>
       </main>
       <Footer />
+      <script
+        src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=Executive%20Office%20of%20the%20President&subagency=USDS"
+        id="_fed_an_ua_tag"
+      ></script>
     </>
   );
 };
