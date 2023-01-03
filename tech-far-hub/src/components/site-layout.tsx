06f99f4fd6a5de4a372322f96be337153015a341
matt bowen <matt@mattbowen.net>
2023-01-03 13:27:17 -0500

    v3 94 Add touchpoints

--- tech-far-hub/src/components/site-layout.tsx
+++ tech-far-hub/src/components/site-layout.tsx
@@ -65,6 +65,7 @@ const SiteLayout = ({ children, breadCrumbs, className }: ILayoutProps) => {
         src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=Executive%20Office%20of%20the%20President&subagency=USDS"
         id="_fed_an_ua_tag"
       ></script>
+      <script src="https://touchpoints.app.cloud.gov/touchpoints/e93bb39d.js"></script>
     </>
   );
 };
