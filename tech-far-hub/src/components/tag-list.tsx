807273704d47a63534a8e0f28032fcbaf26f3a30
matt bowen <matt@mattbowen.net>
2023-01-18 14:12:22 -0500

    v3 115 tags do their own prefixing?

--- tech-far-hub/src/components/tag-list.tsx
+++ tech-far-hub/src/components/tag-list.tsx
@@ -1,3 +1,4 @@
+import { withPrefix } from "gatsby";
 import _ from "lodash";
 import * as React from "react";
 import { Hyperlink } from "./hyperlink";
@@ -6,9 +7,9 @@ export const TagList = ({ tags }: { tags: readonly (string | null)[] }): JSX.Ele
   if (tags && tags.length) {
     const tagList = tags.map((tag) => {
       if (tag) {
-        const urlTag = _.kebabCase(tag);
+        const urlTag = withPrefix(`/tags/${_.kebabCase(tag)}`);
         return (
-          <Hyperlink href={`/tags/${urlTag}`} key={`tag-${tag}`}>
+          <Hyperlink href={urlTag} key={`tag-${tag}`}>
             {tag}
           </Hyperlink>
         );
