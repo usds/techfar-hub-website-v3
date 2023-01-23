263f7b90513119fb2bb336bf3da7e470a98f0b76
matt bowen <matt@mattbowen.net>
2023-01-23 14:41:55 -0500

    v3 57 Improve accesability of process list, keys

--- tech-far-hub/src/components/multi-step.tsx
+++ tech-far-hub/src/components/multi-step.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { Alert, Card, CardBody, CardGroup } from "@trussworks/react-uswds";
+import { v4 as uuidv4 } from "uuid";
 
 export const MultiStep = ({ children }: { children: React.ReactElement }) => {
   const errorMessage = (
@@ -13,7 +14,7 @@ export const MultiStep = ({ children }: { children: React.ReactElement }) => {
   if (children.type !== "ol") {
     return errorMessage;
   }
-
+  const multiStepId = uuidv4();
   const elementFilter = (item: React.ReactNode) => item && typeof item === "object" && "type" in item;
   let items = children.props.children.filter(elementFilter);
 
@@ -36,40 +37,55 @@ export const MultiStep = ({ children }: { children: React.ReactElement }) => {
   items.forEach((li: React.ReactNode, index: number) => {
     if (li && typeof li === "object" && "props" in li && "children" in li.props) {
       const filteredChildren = li.props.children.filter(elementFilter);
+
       headings.push(
         <li
           className={`usa-step-indicator__segment ${
             isVisible[index] ? "usa-step-indicator__segment--current" : "usa-step-indicator__segment--complete"
           }`}
-          key={`multistep-heading-${index}`}
-          onClick={() => handleClick(index)}
+          key={`multistep-${multiStepId}-heading-${index}`}
         >
-          <div className="usa-step-indicator__segment-label" key={`multistep-indicator-${index}`}>
-            {filteredChildren[0]}
+          <div className="usa-step-indicator__segment-label" key={`multistep-${multiStepId}--indicator-${index}`}>
+            {!isVisible[index]  && <a
+              key={`multistep-${multiStepId}-heading-control-${index}`} 
+              href="#" 
+              onClick={(event) => {
+              handleClick(index);
+              event.preventDefault();
+            }}> {filteredChildren[0]}</a>}
+            {isVisible[index] && filteredChildren[0]} 
           </div>
         </li>
       );
       bodies.push(
-        <>
+        <div key={`multistep-${multiStepId}-body-wrapper-${index}`}>
           {isVisible[index] && (
-            <div key={`multistep-body-${index}`} className="tfh-multistep__content-item">
+            <div key={`multistep-${multiStepId}-body-${index}`} className="tfh-multistep__content-item">
               {filteredChildren.slice(1)}
             </div>
           )}
-        </>
+        </div>
       );
     }
   });
 
   return (
     <CardGroup>
-      <Card key="tfh-multistep-wrapper">
+      <Card key={`tfh-multistep-${multiStepId}-wrapper`}>
         <CardBody>
-          <div className="tfh-multistep">
-            <div className="usa-step-indicator" aria-label="progress">
-              <ol className="usa-step-indicator__segments">{headings}</ol>
+          <div className="tfh-multistep" key={`tfh-multistep-${multiStepId}`}>
+            <div
+              className="usa-step-indicator"
+              aria-label="progress"
+              key={`tfh-multistep-indicator-wrapper-${multiStepId}`}
+            >
+              <ol className="usa-step-indicator__segments" key={`tfh-multistep-${multiStepId}-headings-container`}>
+                {headings}
+              </ol>
             </div>
-            <div className="tfh-multipstep__content-block">{bodies}</div>
+            <div className="tfh-multipstep__content-block" key={`tfh-multistep-${multiStepId}-bodies-container`}>
+              {bodies}
+            </div>
           </div>
         </CardBody>
       </Card>
