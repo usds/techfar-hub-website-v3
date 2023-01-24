09ed2a2219128e887544dc33fd7865642b969862
matt bowen <matt@mattbowen.net>
2023-01-24 12:00:50 -0500

    v3 17 Add skelton of contact form

---
+++ tech-far-hub/src/pages/contact.tsx
@@ -0,0 +1,23 @@
+import { HeadFC } from "gatsby";
+import * as React from "react";
+import SiteLayout from "../components/site-layout";
+
+const ContactPage: React.FC = () => {
+  return (
+    <SiteLayout pagePath="/contact">
+      <h1>Contact the TechFAR Hub Team at USDS</h1>
+      <div id="touchpoints-contact"></div>
+      <script src="https://touchpoints.app.cloud.gov/touchpoints/fbdc7110.js" async></script>
+    </SiteLayout>
+  );
+};
+
+export default ContactPage;
+
+export const Head: HeadFC = () => {
+  return (
+    <>
+      <title>Contact | TechFAR Hub</title>
+    </>
+  );
+};
