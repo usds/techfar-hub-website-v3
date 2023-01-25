baf4be9af3296d2ebe73cf05aa0795f41bde743c
matt bowen <matt@mattbowen.net>
2023-01-24 20:11:43 -0500

    v3 17 Fix bad markup from summary box

--- tech-far-hub/src/components/summary.tsx
+++ tech-far-hub/src/components/summary.tsx
@@ -6,7 +6,7 @@ export const Summary = ({ children, heading }: { children: React.ReactNode; head
     <div className="usa-alert usa-alert--info">
       <div className="usa-alert__body">
         <h4 className="usa-alert__heading">{heading}</h4>
-        <p className="usa-alert__text">{children}</p>
+        <div className="usa-alert__text">{children}</div>
       </div>
     </div>
   );
